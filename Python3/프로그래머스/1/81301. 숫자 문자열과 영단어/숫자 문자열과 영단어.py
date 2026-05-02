cc260ca8305c5413cd07e7799e910fda51313a72
KIM MILIM <53042642+KimMilim@users.noreply.github.com>
2026-05-02 14:43:09 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,14 @@
+digit_dict= {"zero": 0, "one":1, "two":2, "three":3, "four":4, "five":5,"six":6, "seven":7,"eight":8,"nine":9}
+
+def solution(s):
+    answer = ""
+    tmp = ""
+    for c in s:
+        if c.isdigit():
+            answer += c
+        else:
+            tmp += c
+            if tmp in digit_dict: 
+                answer += str(digit_dict[tmp])
+                tmp = "" 
+    return int(answer)
