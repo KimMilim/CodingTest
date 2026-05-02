94e8baddeaeea53ccf9e19af4b2b4e1d29a262f1
KIM MILIM <53042642+KimMilim@users.noreply.github.com>
2026-05-02 17:08:18 +0900

    Refactor multi-set creation in solution function
    Refactor list comprehension for multi-set generation and remove redundant comments.

--- Python3/프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링_v2.py
+++ Python3/프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링_v2.py
@@ -1,20 +1,17 @@
 from collections import Counter
 
 def solution(str1, str2):
-    # 1. 리스트 컴프리헨션으로 다중집합 생성
-    list1 = [str1[i:i+2].lower() for i in range(len(str1)-1) if str1[i:i+2].isalpha()]
-    list2 = [str2[i:i+2].lower() for i in range(len(str2)-1) if str2[i:i+2].isalpha()]
     
-    # 2. Counter를 이용한 빈도수 계산
-    c1 = Counter(list1)
+    list1=[str1[i:i+2].lower() for i in range(len(str1)-1) if str1[i:i+2].isalpha() ]
+    list2=[str2[i:i+2].lower() for i in range(len(str2)-1) if str2[i:i+2].isalpha() ]
+    
+    c1 = Counter(list1) # ['aa', 'aa', 'bb']였다면 {'aa': 2, 'bb': 1}
     c2 = Counter(list2)
+
+    inter_len = sum((c1 & c2).values()) # c1 = {'aa': 2, 'bb': 1}, c2 = {'aa': 3, 'bb': 0} 이라면 c1 & c2는 {'aa': 2}
+    union_len = sum((c1 | c2).values()) # c1 | c2는 {'aa': 3, 'bb': 1}
     
-    # 3. Counter의 교집합(&)과 합집합(|) 연산 활용
-    # elements()는 빈도수만큼 요소를 반복하는 이터레이터를 반환합니다.
-    inter_len = sum((c1 & c2).values())
-    union_len = sum((c1 | c2).values())
     
-    # 4. 예외 처리 및 결과 계산
     if union_len == 0:
         return 65536
     
