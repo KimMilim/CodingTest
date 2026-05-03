5f3ed1f5b7939688a7253690f44389b300545f07
KIM MILIM <53042642+KimMilim@users.noreply.github.com>
2026-05-03 17:50:36 +0900

    [level 2] Title: 주차 요금 계산, Time: 4.10 ms, Memory: 9.47 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -0,0 +1,66 @@
+import math
+
+
+def transform(time):
+    h,m=time.split(':')
+    res = int(h)*60+int(m)
+    
+    return res
+
+
+def solution(fees, records):
+    answer = []
+    
+    new_records = [ele.split(" ") for ele in records]
+    new_records.sort(key = lambda x: (x[1], x[0]))
+    
+    check = [0]*len(records)
+    
+    in_time=0
+    i=0
+    time_dict={}
+    cnt=0
+    while i< len(new_records):
+        # if cnt >= 10: break
+        cur = new_records[i]
+        
+        if cur[2]== "IN":
+            if i+1 < len(new_records):
+                next = new_records[i+1]
+                if cur[1] == next[1] and next[2]=="OUT":
+                    if cur[1] in time_dict:
+                        time_dict[cur[1]] += transform(next[0])-transform(cur[0])
+                    else: 
+                        time_dict[cur[1]] = transform(next[0])-transform(cur[0])
+                    i+=2
+                else:
+                    if cur[1] in time_dict:
+                        time_dict[cur[1]] += transform("23:59")-transform(cur[0])
+                    else: 
+                        time_dict[cur[1]] = transform("23:59")-transform(cur[0])
+                    i+=1
+            else: # 다음이 없음
+                if cur[1] in time_dict:
+                        time_dict[cur[1]] += transform("23:59")-transform(cur[0])
+                else: 
+                        time_dict[cur[1]] = transform("23:59")-transform(cur[0])
+                break
+                
+        else:
+            i+=1
+        # cnt+=1
+    
+    for c,t in time_dict.items():
+        if t <= fees[0]:
+            total_fees = fees[1]
+        else:
+            total_fees = fees[1]
+            t-=fees[0]
+            total_fees += math.ceil(t/fees[2])*fees[3]
+        answer.append(total_fees)
+        # print("car: ", c, "fees: ", total_fees)
+            
+                
+    
+    
+    return answer
