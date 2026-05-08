1fb24763d12a7382527b000595299ab0709ee391
KIM MILIM <53042642+KimMilim@users.noreply.github.com>
2026-05-08 15:49:08 +0900

    [level 2] Title: 소수 찾기, Time: 19.74 ms, Memory: 9.35 MB -BaekjoonHub

--- Python3/프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ Python3/프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,34 +1,22 @@
+from itertools import permutations
+
 def is_prime(num):
     if num <=1:
         return False
     i=2
-    while i <= num**0.5:
+    while i <= int(num**0.5):
         if num%i==0:
             return False
         i+=1
     return True
 
-res_set=set()
-def make_number(cur, numbers, visit):
-
-    if cur and is_prime(int(cur)):
-        global res_set
-        res_set.add(int(cur))
-        
-    if sum(visit)==len(numbers): return
-    
-    for i in range(len(numbers)):
-        if visit[i]: continue
-        visit[i]=1
-        make_number(cur+str(numbers[i]),numbers, visit)
-        visit[i]=0
-    
-    return
-
-
 def solution(numbers):
     
-    visit = [0]*len(numbers)
-    make_number("",numbers, visit)
+    res=set()
     
-    return len(res_set)
+    for i in range(len(numbers)):
+        res |= set(map(int, map("".join,permutations(numbers,i+1))))
+        
+    ans = sum(1 for ele in res if is_prime(ele))
+    
+    return ans
