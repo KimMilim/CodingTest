0a906dda4345dbeb5568a0ccf8e0bd08b5fc0853
KIM MILIM <53042642+KimMilim@users.noreply.github.com>
2026-05-11 16:25:11 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.21 ms, Memory: 9.15 MB -BaekjoonHub

--- Python3/프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ Python3/프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,27 +1,3 @@
 def solution(survey, choices):
     answer = ''
-    
-    
-    index = {'RT':0, 'TR':0, 'CF':1, 'FC':1, 'JM':2, 'MJ':2, 'AN':3, 'NA':3}
-    score = [{'R':0, 'T':0}, {'C':0, 'F':0}, {'J':0, 'M':0}, {'A':0, 'N':0}]
-    
-    for i in range(len(survey)):
-        type = survey[i]
-        num = choices[i]
-        
-        if num <= 4:
-            score[index[type]][type[0]] += (4-num)
-        else:
-            score[index[type]][type[1]] += (num-4)
-    
-    for type in score:
-        
-        tmp = list(type.items())
-        # print(tmp)
-        tmp.sort(key=lambda x:(-x[1],x[0]))
-        answer+=tmp[0][0]
-            
-        
-    
-    
     return answer
