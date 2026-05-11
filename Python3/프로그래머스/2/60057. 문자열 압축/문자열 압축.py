280605cd5c66846c4c9f1453f883a1cce80cfe4b
KIM MILIM <53042642+KimMilim@users.noreply.github.com>
2026-05-11 23:12:50 +0900

    [level 2] Title: 문자열 압축, Time: 3.98 ms, Memory: 9.13 MB -BaekjoonHub

--- Python3/프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ Python3/프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,36 +1,20 @@
-def solution(s):
-    answer = 0
-    
-    min_s = s
-    for i in range(1,len(s)//2+1):
-        words=[]
-        
-        j=0
-        while j < len(s):
-            words.append(s[j:j+i])
-            j+=i
-        
-        tmp=""
-        pre=""
-        cnt=0
-        for w in words:
-            if pre != w:
-                if cnt > 1:
-                    tmp+= (str(cnt)+pre)
-                else:
-                    tmp+= pre
-                pre=w
-                cnt=1
-            else:
-                cnt+=1
-        if cnt > 1:
-            tmp+= (str(cnt)+pre)
-        else:
-            tmp+= pre
+from itertools import groupby
 
-        if len(tmp) < len(min_s):
-            min_s=tmp
-            # print(tmp)
-                
+def solution(s):
+    n = len(s)
+    if n == 1:
+        return 1
+    
+    def compressed_length(size):
+        # 1) size 크기로 자르기
+        chunks = [s[i:i+size] for i in range(0, n, size)]
         
-    return len(min_s)
+        # 2) 연속된 같은 조각을 그룹핑해서 압축
+        parts = []
+        for chunk, group in groupby(chunks):
+            count = sum(1 for _ in group)
+            parts.append(f"{count}{chunk}" if count > 1 else chunk)
+        
+        return len(''.join(parts))
+    
+    return min(compressed_length(size) for size in range(1, n // 2 + 1))
