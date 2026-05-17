445960b4232a13d35d5226121dac77cbfc2a440e
KIM MILIM <53042642+KimMilim@users.noreply.github.com>
2026-05-17 15:07:19 +0900

    [level 2] Title: 소수 찾기, Time: 3.40 ms, Memory: 9.58 MB -BaekjoonHub

--- Python3/프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ Python3/프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,22 +1,30 @@
 from itertools import permutations
 
-def is_prime(num):
-    if num <=1:
+def is_prime(n):
+    if n <= 1:
         return False
-    i=2
-    while i <= int(num**0.5):
-        if num%i==0:
+    # 2부터 루트 n까지 나누어 떨어지는지 확인
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
             return False
-        i+=1
     return True
 
 def solution(numbers):
+    candidate_numbers = set()
     
-    res=set()
-    
-    for i in range(len(numbers)):
-        res |= set(map(int, map("".join,permutations(numbers,i+1))))
+    # 1. 1개부터 len(numbers)개까지 뽑는 모든 순열을 구함
+    for i in range(1, len(numbers) + 1):
+        permu = permutations(numbers, i)
         
-    ans = sum(1 for ele in res if is_prime(ele))
-    
-    return ans
+        # 2. 각 순열 글자들을 합쳐서 숫자로 변환 후 set에 저장 (중복 자동 제거)
+        for p in permu:
+            num = int("".join(p))
+            candidate_numbers.add(num)
+            
+    # 3. set에 담긴 고유한 숫자들 중 소수의 개수를 카운트
+    answer = 0
+    for num in candidate_numbers:
+        if is_prime(num):
+            answer += 1
+            
+    return answer
